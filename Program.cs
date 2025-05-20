a748bdca09fee2fe97caadf556a869c5bc9dda4e
YasmimAlves01 <yasmimalvesrodrigues16@gmail.com>
2025-05-20 17:28:21 -0300

    Tabuada funcionando e rodando quantas vezes o usuario desejar

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿int numeroTabu;
+string decisao;
+
+Console.WriteLine("Bem-Vindo ao Tabuada Rapida!");
+
+
+
+while (true)
+{
+    Console.WriteLine("Por favor digite o numero que deseja:");
+    numeroTabu = Convert.ToInt32(Console.ReadLine()!);
+
+
+    for (int x = 0; x <= 10; x++)
+    {
+        Console.WriteLine($"{numeroTabu} x {x} = {numeroTabu * x}");
+
+    }
+
+    Console.WriteLine("\nDeseja ver outras tabuada?");
+    Console.WriteLine("Digite S para sim e N para não");
+    decisao = Console.ReadLine()!.ToUpper();
+    if (decisao == "S")
+    {
+        Console.WriteLine("\nOk então, vamos continuar :)");
+        Console.Clear();
+        continue;
+    }
+    else
+    {
+        Console.WriteLine("\nVamos encerrar por aqui então");
+        Console.WriteLine("Muito obrigado por utilizar o nosso programa ;)\n");
+        break;
+    }
+
+}
